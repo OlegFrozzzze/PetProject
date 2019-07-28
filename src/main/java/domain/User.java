b4d58dbe06a83f091f4eadc126f49231d053ced6
Oleg Strelnikov <ji900@yandex.ru>
2019-07-28 11:52:12 +0300

    Add annotation for class field: Id, Name, Email,Address

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -1,17 +1,20 @@
 package domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column
     private String name;
-
+    @Column
     private String email;
-    
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id")
     private Address address;
 }
