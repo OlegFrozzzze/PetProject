0473bc8728102128e9c5742210137705e8e28146
Oleg Strelnikov <ji900@yandex.ru>
2019-07-28 13:29:12 +0300

    Add Getter and Setter for class field

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -17,4 +17,36 @@ public class User {
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "address_id")
     private Address address;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
 }
