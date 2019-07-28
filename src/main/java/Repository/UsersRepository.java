1e5aa039ea445869fddecdbd71bf35c87d05e33e
Oleg Strelnikov <ji900@yandex.ru>
2019-07-28 22:18:40 +0300

    Here I created my own Sql queries and changed class User to Users

--- src/main/java/Repository/UsersRepository.java
+++ src/main/java/Repository/UsersRepository.java
@@ -1,6 +1,18 @@
 package Repository;
 
-import domain.User;
+import domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface UsersRepository extends JpaRepository<User, Long> {}
+import java.util.List;
+
+public interface UsersRepository extends JpaRepository<Users, Long> {
+    List<Users> findAllByName(String name);
+
+    @Query(nativeQuery = true, value = "SELECT * FROM Users WHERE email LIKE '%@gmail.com%'")
+    List<Users> findWhereEmailIsGmail();
+
+    @Query(value = "SELECT * FROM Users WHERE name LIKE '%Smith%'", nativeQuery = true)
+    List<Users> findWehereNameStartsFromSmith();
+
+}
